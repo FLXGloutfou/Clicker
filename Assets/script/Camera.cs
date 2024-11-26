4f508b15fa50ab3e46fdab4c2ab393fe4d7fd30d
FLXGloutfou <felix.hautin@hotmail.fr>
2024-11-26 09:59:27 +0100

    cam fonctionne

--- Assets/script/Camera.cs
+++ Assets/script/Camera.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Camera : MonoBehaviour
@@ -10,17 +12,57 @@ public class Camera : MonoBehaviour
     private float speed;
 
     [SerializeField]
-    private Transform targetPostition;
+    private Transform woodPos;
+
+    [SerializeField]
+    private Transform shopPos;
+
+
+    private bool isMoving;
+
+    public void GoRight()
+    {
+        if (!isMoving)
+        {
+            StartCoroutine(MoveRight());
+        }
+
+    }
+    private IEnumerator MoveRight()
+    {
+        isMoving = true;
+
+
+        while (Vector3.Distance(transform.position, shopPos.position) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, shopPos.position, speed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = shopPos.position;
+        isMoving = false;
+    }
 
     public void GoLeft()
     {
-        Debug.Log("onbouge");
-        while (transform.position != targetPostition.position)
-        {   
-            Vector3 dir = targetPostition.position - targetPostition.position;
-            transform.Translate(dir * speed * Time.deltaTime);
+        if (!isMoving)
+        {
+            StartCoroutine(MoveLeft());
         }
         
     }
+    private IEnumerator MoveLeft()
+    {
+        isMoving = true;
 
+        
+        while (Vector3.Distance(transform.position, woodPos.position) > 0.1f)
+        {     
+            transform.position = Vector3.MoveTowards(transform.position, woodPos.position, speed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = woodPos.position;
+        isMoving = false;
+    }
+
+    
 }
