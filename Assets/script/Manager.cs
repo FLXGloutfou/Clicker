3cab8fa9795d19b3148844b9d70ab5290ebe123d
FLXGloutfou <felix.hautin@hotmail.fr>
2024-12-06 16:56:10 +0100

    architecture

--- Assets/script/Manager.cs
+++ Assets/script/Manager.cs
@@ -3,59 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Manager : MonoBehaviour
 {
-    public int wood;
-    public Text woodText;
+    public static Manager Instance;
 
-    public int money;
-    public Text moneyText;
+    private void Awake()
+    {
+        if (Manager.Instance == null)
+        {
+            Manager.Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
-    public int woodPerMinute;
-    public Text woodPerMinuteText;
 
-    private int nombreOfAuto;
-    public Text numberOfAutoText;
-
-
+    public int _wood = 0;
+    public int _money = 0;
+    public int _woodPerMinute;
+    public int _nombreOfAuto;
+    public int _autoWoodCost;
+    public int _autoValueCost;
     
+
     private bool isAutoClicking = false;
     private int autoWoodValue;
-    private int autoWoodCost;
 
 
 
     void Start ()
     {
-        money = 0;
-        wood = 0;
-        nombreOfAuto = 0;
+        _nombreOfAuto = 0;
         autoWoodValue = 10;
-        autoWoodCost = 100;
+        _autoWoodCost = 100;
+        _autoValueCost = 100;
     }
 
     void Update()
     {
-        woodText.text = wood.ToString();
-        moneyText.text = money.ToString();
-        woodPerMinuteText.text = woodPerMinute.ToString();
-        numberOfAutoText.text = nombreOfAuto.ToString();
 
     }
-
+    public void SetWood(int newAmount)
+    {
+        _wood = newAmount;
+        //feedback
+    }
     public void AddWood(int amount)
     {
-        wood += amount;
+        SetWood(_wood+amount);
     }
 
+
+    public void ButtonSellWood()
+    { 
+        _money = _money + _wood * 10;
+        _wood = 0;
+    }
 
     public void ButtonAutoWood()
     {
-        if (wood > autoWoodCost)
+        if (_money > _autoWoodCost)
         {
-            autoWoodCost *= 2;
-            wood -= autoWoodCost;
-            nombreOfAuto++;
+            _money -= _autoWoodCost;
+            var tempAutoWood = (float)_autoWoodCost;
+            tempAutoWood *= 1.5f;
+            _autoWoodCost = (int)Mathf.Round(tempAutoWood);
+  
+            _nombreOfAuto++;
             if (!isAutoClicking)
             {
                 StartCoroutine(createAutoClic());
@@ -69,14 +86,30 @@ public class Manager : MonoBehaviour
         
         isAutoClicking = true;
 
-        while (nombreOfAuto > 0)
+        while (_nombreOfAuto > 0)
         {
-            woodPerMinute = nombreOfAuto * autoWoodValue;
-            AddWood(woodPerMinute);
+            _woodPerMinute = _nombreOfAuto * autoWoodValue;
+            AddWood(_woodPerMinute);
             yield return new WaitForSeconds(1f);
         }
 
         isAutoClicking = false;
+    }
+
+
+
+    public void AutoValueIncrease()
+    {
+        if (_money > _autoValueCost)
+        {
+            _money -= _autoValueCost;
+
+            var tempAutoValue = (float)_autoValueCost;
+            tempAutoValue *= 1.5f;
+            _autoValueCost = (int)Mathf.Round(tempAutoValue);
+
+            autoWoodValue += 10;
+        }
     }
 }
 
--- Assets/script/Ui.cs
+++ Assets/script/Ui.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ui : MonoBehaviour
 {
 
     public GameObject upgradewindow;
     private bool Upgradeisopen = false;
+
+    public Text woodText;
+    public Text moneyText;
+    public Text woodPerMinuteText;
+    public Text numberOfAutoText;
+    public Text autoWoodCostText;
+    public Text autoValueCostText;
 
     public void OpenUpgradeWindow()
     {
@@ -22,6 +30,16 @@ public class Ui : MonoBehaviour
             Upgradeisopen = false;
         }
         
+    }
+
+    void Update()
+    {
+        woodText.text = Manager.Instance._wood.ToString();
+        moneyText.text = Manager.Instance._money.ToString();
+        woodPerMinuteText.text = Manager.Instance._woodPerMinute.ToString();
+        numberOfAutoText.text = Manager.Instance._nombreOfAuto.ToString();
+        autoWoodCostText.text = Manager.Instance._autoWoodCost.ToString();
+        autoValueCostText.text = Manager.Instance._autoValueCost.ToString();
     }
 
 }
