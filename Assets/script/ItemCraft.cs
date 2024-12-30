e4d23d49d90261b55404b5911ce05bf26cafe3ba
FLXGloutfou <felix.hautin@hotmail.fr>
2024-12-30 23:01:49 +0100

    V1

--- Assets/script/ItemCraft.cs
+++ Assets/script/ItemCraft.cs
@@ -11,6 +11,8 @@ public class ItemCraft : MonoBehaviour
     public Text _stockUI;
     public Image _iconUI;
 
+    private bool _isSelling = false;
+
     public void CurrentSlot(FurnitureSlot slot)
     {
         UpdateSlotUI(slot);
@@ -23,13 +25,30 @@ public class ItemCraft : MonoBehaviour
             _currentslot._plan._stock += 10;
             Manager.Instance._wood -= _currentslot._plan._woodCost;
 
+            if (!_isSelling)
+            {
+                _stockUI.text = _currentslot._plan._stock.ToString();
+                StartCoroutine(SellFromStock());
+            }
             
-            _stockUI.text = _currentslot._plan._stock.ToString();
         }
         else
         {
             Debug.Log("pas assez de bois");
         }
+    }
+
+    public IEnumerator SellFromStock()
+    {
+        _isSelling = true;
+        while (_currentslot._plan._stock > 0)
+        {
+            _currentslot._plan._stock--;
+            _stockUI.text = _currentslot._plan._stock.ToString();
+            Manager.Instance._money += _currentslot._plan._sellValue;
+            yield return new WaitForSeconds(1f);
+        }
+        _isSelling = false;
     }
 
     private void UpdateSlotUI(FurnitureSlot slot)
